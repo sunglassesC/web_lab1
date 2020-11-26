93963fe63f95ae7bee9ef68d9cde8abf3b65e1a0
SulleyC <972318234@qq.com>
2020-11-26 13:21:26 +0800

    rename and modify test.py

--- README.md
+++ README.md
@@ -2,9 +2,28 @@
 
 
 
-## 使用语言
+## 运行环境
+
+#### 语言
 
 python
+
+
+
+#### 依赖模块
+
+nltk
+
+
+
+In order to install NLTK run the following commands in your terminal.
+
+- `sudo pip install nltk`
+- Then, enter the python shell in your terminal by simply typing `python`
+- Type `import nltk`
+- `nltk.download(‘all’)`
+
+> [参考](https://www.geeksforgeeks.org/tokenize-text-using-nltk-python/?ref=lbp)
 
 
 
@@ -18,6 +37,7 @@ python
 
 - 去停用词
 
-- 得到倒排表
+- 得到倒排表（取词频前1000的词）
 
 倒排表使用python中的字典结构，以单词为键，出现的文档编号组成的数组为值。这里需要说明的是，如果使用邮件的路径作为文档名，类似`./dataset/maildir\allen-p\all_documents\2`这样的字符串，占用空间太多，且由于文档和词的关系是一对多，倒排表中文档名会大量出现，故对所有文档进行编号，将编号和路径的映射输出并存储到`output`文件中，在后续查询时可先得到文档编号，后根据编号及映射表找到文档路径。
+
--- build_posting_list.py
+++ build_posting_list.py
@@ -2,7 +2,7 @@ import os
 from email.parser import Parser
 from email.policy import default
 import json
-# from nltk.tokenize import sent_tokenize, word_tokenize
+from nltk.tokenize import sent_tokenize, word_tokenize
 from nltk.corpus import stopwords
 from nltk.stem import PorterStemmer
 from nltk.tokenize import RegexpTokenizer
@@ -11,7 +11,7 @@ from tqdm import tqdm
 
 
 if __name__ == '__main__':
-    file_num_list = ['']    # 文件名和序号的映射，0号元素为一个空的字符串
+    file_num_list = ['']    # 文件名和序号的映射，0号元素为一个空的字符串，即文件编号从1开始
     posting_list = {}       # 倒排表
     data_root_path = r"./dataset/maildir"
 
@@ -25,7 +25,7 @@ if __name__ == '__main__':
     seq_num = 0  # 文件名对应序号
     for root, dirs, files in tqdm(os.walk(data_root_path), total=files_count):
         for file in files:
-            seq_num += 1
+            seq_num += 1        # 文件序号从1开始
 
             file_path = os.path.join(root, file)
             file_num_list.append(file_path)
@@ -40,31 +40,46 @@ if __name__ == '__main__':
                     current_email = Parser(policy=default).parsestr(text)
                     body = current_email.get_payload()          # 邮件正文
                     subject_text = current_email['Subject']     # 邮件主题
+                    body += ' ' + subject_text                  # 主题并入正文，不作特殊处理
 
                     # 分词
-                    tokenizer = RegexpTokenizer(r'\w+')
-                    word_tokens = tokenizer.tokenize(body)
+                    word_tokens = word_tokenize(body)
 
                     # 词根化
                     ps = PorterStemmer()
-                    stemmed_tokens = [ps.stem(w) for w in word_tokens]
+                    stemmed_tokens = [ps.stem(w) for w in word_tokens if w.isalpha()]
 
                     # 去停用词
                     stop_words = set(stopwords.words('english'))
-                    filtered_tokens = [w for w in word_tokens if w not in stop_words]
+                    filtered_tokens = [w for w in stemmed_tokens if w.lower() not in stop_words]
 
                     for token in filtered_tokens:
                         if token not in posting_list:
-                            posting_list[token] = {seq_num: 1}
+                            posting_list[token] = {
+                                'freq': 1,                      # 词频
+                                'distribution': {seq_num: 1}    # 分布，格式为  文档编号: 出现次数
+                            }
                         else:
-                            if seq_num not in posting_list[token]:
-                                posting_list[token][seq_num] = 1
+                            posting_list[token]['freq'] += 1
+                            if seq_num not in posting_list[token]['distribution']:
+                                posting_list[token]['distribution'][seq_num] = 1
                             else:
-                                posting_list[token][seq_num] += 1
+                                posting_list[token]['distribution'][seq_num] += 1
 
+    first_1000_token = {}
+    for i in range(1000):
+        if posting_list:
+            a = max(posting_list.items(), key=lambda x: x[1]['freq'])
+            first_1000_token[a[0]] = a[1]
+            del posting_list[a[0]]
+        else:
+            print('list is already empty')
+            break
+
+    # print(first_1000_token)
     print('开始写入output')
     with open('./output/postinglist.txt', 'w') as f:
-        f.write(json.dumps(posting_list))
+        f.write(json.dumps(first_1000_token))
 
     # 将文件名和序号的映射写入filename_map中
     with open('./output/filename_map.txt', 'w') as f:
